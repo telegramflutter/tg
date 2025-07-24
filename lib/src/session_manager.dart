fdefb0e7ac7f6a2ea86ddcc95893076ce51d723c
ilkham khabibullin <mailbox.ilham@gmail.com>
2025-07-24 18:59:29 +0300

    session manager accepts auth key as constructor argument

--- lib/src/session_manager.dart
+++ lib/src/session_manager.dart
@@ -1,15 +1,9 @@
 part of '../tg.dart';
 
-class SessionInfo {
-  SessionInfo({
-    required this.authorizationKey,
-    required this.seqno,
+abstract class SessionInfoManager {
+  SessionInfoManager({
+    required AuthorizationKey authorizationKey,
   });
 
-  final AuthorizationKey authorizationKey;
-  final String seqno;
-}
-
-abstract class SessionInfoManager {
   Future<void> updateSeqno(int id, int seqno);
 }
