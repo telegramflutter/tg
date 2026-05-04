4a1833ab18b67bc552873ab89174b9b5ed7d985b
Deekshith SH <shdeekshith@gmail.com>
2026-05-04 20:35:14 +0530

    Handle BadServerSalt

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,5 +1,12 @@
 part of '../tg.dart';
 
+class _PendingTask {
+  final Completer<t.Result> completer;
+  final t.TlMethod method;
+
+  _PendingTask(this.completer, this.method);
+}
+
 class Client extends t.Client {
   Client({
     required this.socket,
@@ -44,14 +51,14 @@ class Client extends t.Client {
     return ak;
   }
 
-  final AuthorizationKey authorizationKey;
+  AuthorizationKey authorizationKey;
   final Obfuscation obfuscation;
   final SocketAbstraction socket;
   final MessageIdGenerator idGenerator;
 
   late final _EncryptedTransformer _transformer;
 
-  final Map<int, Completer<t.Result>> _pending = {};
+  final Map<int, _PendingTask> _pendingTasks = {};
 
   final _streamController = StreamController<UpdatesBase>.broadcast();
 
@@ -64,30 +71,28 @@ class Client extends t.Client {
       _streamController.add(msg);
     }
 
-    //
     if (msg is MsgContainer) {
       for (final message in msg.messages) {
         _handleIncomingMessage(message);
       }
-
       return;
     } else if (msg is Msg) {
       _handleIncomingMessage(msg.body);
       return;
     } else if (msg is BadMsgNotification) {
       final badMsgId = msg.badMsgId;
-      final task = _pending[badMsgId];
-      task?.completeError(BadMessageException._(msg));
-      _pending.remove(badMsgId);
+      final task = _pendingTasks[badMsgId];
+      task?.completer.completeError(BadMessageException._(msg));
+      _pendingTasks.remove(badMsgId);
     } else if (msg is RpcResult) {
       final reqMsgId = msg.reqMsgId;
-      final task = _pending[reqMsgId];
+      final task = _pendingTasks[reqMsgId];
 
       final result = msg.result;
 
       if (result is RpcError) {
-        task?.complete(t.Result.error(result));
-        _pending.remove(reqMsgId);
+        task?.completer.complete(t.Result.error(result));
+        _pendingTasks.remove(reqMsgId);
         return;
       } else if (result is GzipPacked) {
         final gZippedData = GZipDecoder().decodeBytes(result.packedData);
@@ -100,21 +105,40 @@ class Client extends t.Client {
         return;
       }
 
-      task?.complete(t.Result.ok(msg.result));
-      _pending.remove(reqMsgId);
+      task?.completer.complete(t.Result.ok(msg.result));
+      _pendingTasks.remove(reqMsgId);
     } else if (msg is GzipPacked) {
       final gZippedData = GZipDecoder().decodeBytes(msg.packedData);
       final newObj = BinaryReader(Uint8List.fromList(gZippedData)).readObject();
       _handleIncomingMessage(newObj);
+    } else if (msg is BadServerSalt) {
+      authorizationKey = AuthorizationKey(
+        authorizationKey.id,
+        authorizationKey.key,
+        msg.newServerSalt,
+      );
+
+      final badMsgId = msg.badMsgId;
+      final task = _pendingTasks.remove(badMsgId);
+
+      if (task != null) {
+        _invoke(task.method, task.completer);
+      }
     }
   }
 
   @override
-  Future<t.Result<t.TlObject>> invoke(t.TlMethod method) async {
+  Future<t.Result<t.TlObject>> invoke(t.TlMethod method) {
+    return _invoke(method, Completer<t.Result>());
+  }
+
+  Future<t.Result<t.TlObject>> _invoke(
+    t.TlMethod method,
+    Completer<t.Result> completer,
+  ) async {
     final preferEncryption = authorizationKey.id != 0;
     final msgsToAck = authorizationKey._msgsToAck;
 
-    final completer = Completer<t.Result>();
     final m = idGenerator._next(preferEncryption);
 
     if (preferEncryption && msgsToAck.isNotEmpty) {
@@ -124,18 +148,8 @@ class Client extends t.Client {
 
       final container = MsgContainer(
         messages: [
-          Msg(
-            msgId: m.id,
-            seqno: m.seqno,
-            bytes: 0,
-            body: method,
-          ),
-          Msg(
-            msgId: ack.id,
-            seqno: ack.seqno,
-            bytes: 0,
-            body: ackMsg,
-          )
+          Msg(msgId: m.id, seqno: m.seqno, bytes: 0, body: method),
+          Msg(msgId: ack.id, seqno: ack.seqno, bytes: 0, body: ackMsg),
         ],
       );
 
@@ -148,7 +162,7 @@ class Client extends t.Client {
       //return invoke(container, false);
     }
 
-    _pending[m.id] = completer;
+    _pendingTasks[m.id] = _PendingTask(completer, method);
     final buffer = authorizationKey.id == 0
         ? _encodeNoAuth(method, m)
         : _encodeWithAuth(method, m, _sessionId, authorizationKey);
